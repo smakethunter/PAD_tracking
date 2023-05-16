78fe78f33ba2e5deb299844947605be06fe8969f
smaket <wojtek.pelka@gmail.com>
2023-05-16 09:50:29 +0200

    initial

---
+++ main.py
@@ -0,0 +1,194 @@
+import os
+from typing import List
+
+import cv2
+import matplotlib.pyplot as plt
+
+from models.model import inception_iccv
+import numpy as np
+from models.model import datasets, detect_pedestrians
+import torchvision.transforms as transforms
+from PIL import Image
+import torch
+global args
+from models.model.datasets import description, attr_nums
+# create model
+model_inception = inception_iccv(pretrained=True, num_classes=35)
+model_rap = inception_iccv(pretrained=True, num_classes=attr_nums['rap'])
+# get the number of model parameters
+# print('Number of model parameters: {}'.format(
+#     sum([p.data.nelement() for p in model_inception.parameters()])))
+
+# for training on multiple GPUs.
+# model_path = "./models/model/peta_epoch_31.pth.tar"
+model_path = "./models/model/rap_epoch_9.pth.tar"
+# checkpoint = torch.load(model_path, map_location=torch.device('cpu') )
+# start_epoch = checkpoint['epoch']
+# best_accu = checkpoint['best_accu']
+# model_inception.load_state_dict(checkpoint['state_dict'])
+# optionally resume from a checkpoint
+
+
+class PeopleReIdentificator:
+    def __init__(self, detector=None, classifier=None, class_threshold=0.55):
+        self._detector = detector if detector is not None else detect_pedestrians.PedestrianSensor()
+        self._classifier = classifier
+        self._objects_buffer: List[objectInfo] = []
+        self.class_threshold = class_threshold
+        self.last_id = 0
+
+    def detect(self, image):
+        # image = cv2.resize(image, (256, 128), interpolation = cv2.INTER_AREA)
+
+        # Detecting all the regions
+        # in the Image that has a
+        # pedestrians inside it
+        (regions, _) = self._detector.nn_detect(image)
+
+        # Drawing the regions in the
+        # Image
+        # (x, y, w, h)
+
+        return regions, _
+
+
+    def get_PAR_features(self, detection_image):
+        model = torch.nn.DataParallel(self._classifier)  # .cuda()
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+        transform_test = transforms.Compose([
+            transforms.Resize(size=(256, 128)),
+            transforms.ToTensor(),
+            normalize
+        ])
+        # input = Image.fromarray(detection_image)
+        # input = input.cuda(non_blocking=True)
+        img = transform_test(detection_image)
+        output = model(img.unsqueeze(0))
+        output = torch.max(torch.max(torch.max(output[0], output[1]), output[2]), output[3])
+        output = torch.sigmoid(output).detach().cpu().numpy()
+        output = np.where(output > self.class_threshold, 1, 0)
+        output = output.reshape((output.shape[1], ))
+        return output
+
+    def _identify(self, frame):
+        detections, _ = self.detect(frame)
+        is_buffer = len(self._objects_buffer) > 0
+        for detection in detections:
+            input = Image.fromarray(cv2.cvtColor(im[detection[0]:detection[0] + detection[3],
+                                                 detection[1]:detection[1] + detection[2]],
+                                                 cv2.COLOR_BGR2RGB))
+            features = self.get_PAR_features(input)
+            detected_object = objectInfo(id=None, features=features, bbox=detection)
+            # if is_buffer:
+            new_object = True
+            if is_buffer:
+                for buff in self._objects_buffer:
+                    if buff == detected_object:
+                        buff.bbox = detected_object.bbox
+                        buff.repeated = True
+                        new_object = False
+                        break
+            if new_object:
+                detected_object.id = self.last_id
+                self._objects_buffer.append(detected_object)
+                self.last_id += 1
+
+        for buff in self._objects_buffer:
+            if buff.lifetime >= buff.time_to_live:
+                self._objects_buffer.remove(buff)
+            if buff.repeated:
+                buff.lifetime = 0
+                buff.repeated = False
+            else:
+                buff.lifetime += 1
+
+    def draw_detections(self, array, detections):
+        for detection in detections:
+            cv2.rectangle(array, (detection[1], detection[0]),
+                          (detection[1] + detection[2], detection[0] + detection[3]), color=(255, 0, 0),
+                          thickness=2)
+    def draw_identifications(self, array):
+        for obj in self._objects_buffer:
+            detection = obj.bbox
+            cv2.rectangle(array, (detection[1], detection[0]),
+                          (detection[1] + detection[2], detection[0] + detection[3]), color=(0, 255, 0),
+                          thickness=2)
+            cv2.putText(array, f"ID: {obj.id}, Lifetime {obj.lifetime}", (detection[1], detection[0]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0),
+                        thickness=2)
+class objectInfo:
+    def __init__(self, id, features, bbox, **kwargs):
+        self.id = id
+        self.features = features
+        self.bbox = bbox
+        self.lifetime = 0
+        self.time_to_live = kwargs.get('time_to_live', 3)
+        self.iou_thr = kwargs.get('iou_threshold', 0.8)
+        self.similarity_thr = kwargs.get('similarity_threshold', 0.7)
+        self.description = description[kwargs.get('dataset', 'rap')]
+        self.repeated = False
+
+
+
+
+    def __eq__(self, other):
+        is_overlaping = self.bb_intersection_over_union(self.bbox, other.bbox) >= self.iou_thr
+        is_similar = self.is_feature_similar(other)
+        return is_overlaping and is_similar
+
+
+
+    def is_feature_similar(self, other):
+        vector_conjunction = (self.features == other.features)
+        return np.sum(vector_conjunction)/vector_conjunction.shape[0] >= self.similarity_thr
+
+    @staticmethod
+    def bb_intersection_over_union(boxA, boxB):
+        # determine the (x, y)-coordinates of the intersection rectangle
+        x1 = max(boxA[0], boxB[0])
+        y1 = max(boxA[1], boxB[1])
+        x2 = min(boxA[0]+boxA[3], boxB[0]+boxB[3])
+        y2 = min(boxA[1] + boxA[2], boxB[1] + boxB[2])
+        # compute the area of intersection rectangle
+        intersection_area = max(x2 - x1, 0) * max(y2 - y1, 0)
+        # compute the area of both the prediction and ground-truth
+        # rectangles
+        union_area = boxA[2] * boxA[3] + boxB[2] * boxB[3] - intersection_area
+
+        # compute the intersection over union by taking the intersection
+        # area and dividing it by the sum of prediction + ground-truth
+        # areas - the interesection area
+        iou = intersection_area / union_area
+        # return the intersection over union value
+        print(boxA, boxB, iou)
+        return iou
+
+    @property
+    def features_string(self):
+        return [self.description[where] for where in np.argwhere(self.features == 1).flatten()]
+
+    def cropped_image(self, image):
+        return image[self.bbox[0]:self.bbox[0]+self.bbox[3], self.bbox[1]:self.bbox[1]+self.bbox[2]]
+
+
+
+
+if __name__ == "__main__":
+    model = PeopleReIdentificator(None, model_rap, 0.6)
+    for i in range(200, 1, -1):
+        im = cv2.imread('./data/MOT20/test/MOT20-06/img1/{0:06d}.jpg'.format(i))
+        # print('./data/MOT20/test/MOT20-06/img1/{0:06d}.jpg'.format(i))
+        im_out = im.copy()
+        detections, _ = model.detect(im)
+        model.draw_detections(im_out, detections)
+        model._identify(im)
+        # print([(m.id, m.bbox) for m in model._objects_buffer])
+        # print("drawing...")
+        model.draw_identifications(im_out)
+        write_name = './data/MOT20/test/MOT20-06/demo/{0:06d}.jpg'.format(i)
+        cv2.imwrite(write_name, im_out)
+
+
+
+
+
---
+++ models/model/detect_pedestrians.py
@@ -0,0 +1,168 @@
+import pickle
+import cv2
+import numpy as np
+from imutils.object_detection import non_max_suppression
+from skimage.feature import hog
+import imutils
+import torchvision.transforms as T
+import torchvision
+class PedestrianSensor(object):
+    def __init__(self):
+        self.pedestrian_view = None
+        self.detection = False
+        self.recording = False
+        # self.clf = pickle.load(open('/Users/smaket/Desktop/siapwpa/pedestrians - carla/clf_svm_pedestrians.bin', 'rb'))
+        self.hog = cv2.HOGDescriptor()
+        self.hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+        # self.sensor.listen(lambda image: PedestrianSensor._parse_mask_pedestrian(weak_self, image))
+        self.nn = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+        self.nn.eval()
+        self.COCO_INSTANCE_CATEGORY_NAMES = [
+            '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+            'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+            'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+            'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+            'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+            'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+            'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+            'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+            'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+            'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+            'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+            'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+        ]
+    def toggle_detection(self):
+        self.detection = not self.detection
+
+    def toggle_recording_detection(self):
+        self.recording = not self.recording
+
+
+    @staticmethod
+    def sliding_window(image, stepSizeX, stepSizeY, windowSize):
+        # slide a window across the image
+        for y in range(0, image.shape[0], stepSizeY):
+            for x in range(0, image.shape[1], stepSizeX):
+                # yield the current window
+                yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
+
+    @staticmethod
+    def pyramid(image, scale=1.5, minSize=(64, 128)):
+        # yield the original image
+        # yield image
+        # keep looping over the pyramid
+        while True:
+            # compute the new dimensions of the image and resize it
+            w = int(image.shape[1] / scale)
+            # h = int(image.shape[0] / scale)
+            image = imutils.resize(image, width=w)
+            # if the resized image does not meet the supplied minimum
+            # size, then stop constructing the pyramid
+            if image.shape[0] < minSize[1] or image.shape[1] < minSize[0]:
+                break
+            # yield the next image in the pyramid
+            yield image
+
+    def pedestrian_detection_svm(self, img):
+        (winW, winH) = (64, 128)
+        biasUp = 150
+        biasDown = 100
+
+        biasLeft = 300
+        biasRight = 10
+        image = np.copy(img)
+        rects = []
+
+        for resized in self.pyramid(image[biasUp:-biasDown, biasLeft:-biasRight, :], scale=1.05):
+            # loop over the sliding window for each layer of the pyramid
+            for (x, y, window) in self.sliding_window(resized, stepSizeX=32, stepSizeY=32, windowSize=(winW, winH)):
+                # if the window does not meet our desired window size, ignore it
+                if window.shape[0] != winH or window.shape[1] != winW:
+                    continue
+                window_r = cv2.resize(window, (32, 64))
+                fd = hog(window_r, orientations=8, pixels_per_cell=(8, 8), cells_per_block=(1, 1), visualize=False, channel_axis=-1)
+                label = self.clf.predict([fd])
+                print(label)
+                if label[0] == "pedestrian":
+                    scale = int(image.shape[0] / resized.shape[0])
+                    xx = x * scale
+                    yy = y * scale
+                    rects.append([xx+biasLeft, yy+biasUp, xx + winW*scale + biasLeft, yy + winH*scale + biasUp])
+                               # else:
+                #     scale = int(image.shape[0] / resized.shape[0])
+                #     xx = x * scale
+                #     yy = y * scale
+                #     cv2.rectangle(image, (xx + biasLeft, yy + biasUp),
+                #                   (xx + winW * scale + biasLeft, yy + winH * scale + biasUp), (255, 0, 0), 2)
+        picks = non_max_suppression(np.array(rects), probs=None, overlapThresh=0.2)
+        for pick in picks:
+            cv2.rectangle(image, (pick[0], pick[1]), (pick[2], pick[3]), (0, 255, 0), 2)
+
+        return image
+
+    def pedestrian_detection_cv2(self, img):
+        image = np.copy(img)
+        (rects, weights) = self.hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+        rects = np.array([[x, y, x+w, y+h] for (x, y, w, h) in rects])
+        pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
+        for (xA, yA, xB, yB) in pick:
+            cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
+        return image
+
+    def nn_detect(self, img):
+        array = img.copy()
+        y_max, x_max, _ = array.shape
+        height, width = 96, 32
+        xywhs = []
+        bboxes, clss = self.get_prediction(array, 0.8)
+        for bbox, cls in zip(bboxes, clss):
+            if cls == "person":
+                bbox = [int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])]
+                xywhs.append([bbox[1], bbox[0], bbox[3] - bbox[1], bbox[2] - bbox[0], cls])
+
+        for detection in xywhs:
+            cv2.rectangle(array, (detection[1], detection[0]),
+                          (detection[1] + detection[3], detection[0] + detection[2]), color=(0, 255, 0), thickness=2)
+            cv2.putText(array, detection[4], (detection[1], detection[0]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0),
+                        thickness=2)
+            cv2.imwrite(f"test{detection[1]}{detection[0]}.jpg", array[detection[0]:detection[0] + detection[2], detection[1]:detection[1] + detection[3]])
+        return [(x,y,h,w) for (x,y,w,h,l) in xywhs], array
+
+    def get_prediction(self, img, threshold):
+        """
+        get_prediction
+          parameters:
+            - img_path - path of the input image
+            - threshold - threshold value for prediction score
+          method:
+            - Image is obtained from the image path
+            - the image is converted to image tensor using PyTorch's Transforms
+            - image is passed through the model to get the predictions
+            - class, box coordinates are obtained, but only prediction score > threshold
+              are chosen.
+        """
+        transform = T.Compose([T.ToTensor()])
+        img = transform(img)
+        pred = self.nn([img])
+        pred_class = [self.COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(pred[0]['labels'].numpy())]
+        pred_boxes = [[i[0], i[1], i[2], i[3]] for i in list(pred[0]['boxes'].detach().numpy())]
+        pred_score = list(pred[0]['scores'].detach().numpy())
+        pred_t = [pred_score.index(x) for x in pred_score if x > threshold][-1]
+        pred_boxes = pred_boxes[:pred_t + 1]
+        pred_class = pred_class[:pred_t + 1]
+        return pred_boxes, pred_class
+
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    img_path = '../../data/1200x-1.jpeg'
+    img = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
+    detector = PedestrianSensor()
+    # img_out = detector.pedestrian_detection_svm(img)
+    regions, img_out = detector.nn_detect(img)
+    fig, ax = plt.subplots()
+    ax.imshow(img_out)
+    fig.show()
